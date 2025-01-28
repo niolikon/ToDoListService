639a7b65ffc8a76cab71e19a9b36f1e0107e0ce3
Simone Andrea Muscas <simoneandrea.muscas@gmail.com>
2025-01-28 08:04:51 +0100

    Add EnsureDatabaseCreated on Program
      TLS2

---
+++ ToDoListService.Api/Extensions/WebApplicationExtensions.cs
@@ -0,0 +1,17 @@
+﻿using ToDoListService.Infrastructure.Repositories;
+
+namespace ToDoListService.Api.Extensions;
+
+public static class WebApplicationExtensions
+{
+    public static WebApplication EnsureDatabaseCreated(this WebApplication webApplication)
+    {
+        using (var scope = webApplication.Services.CreateScope())
+        {
+            var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+            dbContext.Database.EnsureCreated();
+        }
+
+        return webApplication;
+    }
+}
