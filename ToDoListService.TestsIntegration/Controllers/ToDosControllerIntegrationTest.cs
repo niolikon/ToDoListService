9654435e7d3973669c7cf497d2ded5ae5f5ee183
Simone Andrea Muscas <simoneandrea.muscas@gmail.com>
2025-01-28 06:04:19 +0100

    Add Integration Test for Marking Workflow
      TLS1

--- ToDoListService.TestsIntegration/Controllers/ToDosControllerIntegrationTest.cs
+++ ToDoListService.TestsIntegration/Controllers/ToDosControllerIntegrationTest.cs
@@ -1,14 +1,14 @@
 ﻿using FluentAssertions;
 using Microsoft.AspNetCore.Identity;
+using System.Net;
 using System.Net.Http.Json;
+using System.Net.Http.Headers;
 using ToDoListService.Domain.Dtos;
 using ToDoListService.Domain.Entities;
 using ToDoListService.Framework.Utils.Testcontainers;
 using ToDoListService.Framework.Utils.EntityFrameworkCore;
 using ToDoListService.Infrastructure.Repositories;
 using ToDoListService.TestsIntegration.TestData;
-using System.Net.Http.Headers;
-using System.Net;
 using ToDoListService.Framework.Security.Jwt;
 
 namespace ToDoListService.TestsIntegration.Controllers;
@@ -33,11 +33,11 @@ public class ToDosControllerIntegrationTest
     [Fact]
     public async Task Get_All_Should_Return_No_Entities_On_Empty_Repository()
     {
-        var seederCleaner = GetSeederCleaner();
-        seederCleaner.PopulateDatabase(ToDosTestScenarios.Empty);
+        using var seederCleaner = GetSeederCleaner();
+        seederCleaner.PopulateDatabase(ToDoTestScenarios.Empty);
 
         HttpClient httpClient = _webApplicationFactory.CreateClient();
-        UserLoginDto loginDto = ToDosTestScenarios.EmptyOwnerDto;
+        UserLoginDto loginDto = ToDoTestScenarios.EmptyOwnerDto;
         HttpResponseMessage loginPostResponse = await httpClient.PostAsJsonAsync("/api/Users/login", loginDto);
         loginPostResponse.StatusCode.Should().Be(HttpStatusCode.OK);
         loginPostResponse.Content.Should().NotBeNull();
@@ -49,5 +49,62 @@ public class ToDosControllerIntegrationTest
 
         returnedCourses.Should().NotBeNull();
         returnedCourses.Should().BeEmpty();
+    }
+
+    [Fact]
+    public async Task Should_Support_Marking_ToDo_WorkFlow()
+    {
+        using var seederCleaner = GetSeederCleaner();
+        seederCleaner.PopulateDatabase(ToDoTestScenarios.SingleCompleted);
+
+        HttpClient httpClient = _webApplicationFactory.CreateClient();
+        UserLoginDto loginDto = ToDoTestScenarios.SingleCompletedOwnerDto;
+        HttpResponseMessage loginPostResponse = await httpClient.PostAsJsonAsync("/api/Users/login", loginDto);
+        loginPostResponse.StatusCode.Should().Be(HttpStatusCode.OK);
+        loginPostResponse.Content.Should().NotBeNull();
+        JwtTokenDto? userTokenDto = await loginPostResponse.Content.ReadFromJsonAsync<JwtTokenDto>();
+        userTokenDto.Should().NotBeNull();
+        httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", userTokenDto.Token);
+
+        IEnumerable<ToDoOutputDto>? returnedToDos = await httpClient.GetFromJsonAsync<IEnumerable<ToDoOutputDto>>("/api/ToDos");
+        returnedToDos.Should().NotBeNull();
+        returnedToDos.Should().HaveCount(1);
+
+        IEnumerable<ToDoOutputDto>? pendingToDos = await httpClient.GetFromJsonAsync<IEnumerable<ToDoOutputDto>>("/api/ToDos/pending");
+        pendingToDos.Should().NotBeNull();
+        pendingToDos.Should().HaveCount(0);
+
+        HttpResponseMessage todoPostResponse = await httpClient.PostAsJsonAsync("/api/ToDos", ToDoTestData.TODO_1_INPUT);
+        todoPostResponse.StatusCode.Should().Be(HttpStatusCode.Created);
+        todoPostResponse.Content.Should().NotBeNull();
+        todoPostResponse.Headers.Should().ContainKey("Location");
+        ToDoOutputDto? postedToDo = await todoPostResponse.Content.ReadFromJsonAsync<ToDoOutputDto>();
+        postedToDo.Should().NotBeNull();
+        postedToDo.Id.Should().NotBe(0);
+        postedToDo.IsCompleted.Should().BeFalse();
+
+        returnedToDos = await httpClient.GetFromJsonAsync<IEnumerable<ToDoOutputDto>>("/api/ToDos");
+        returnedToDos.Should().NotBeNull();
+        returnedToDos.Should().HaveCount(2);
+
+        pendingToDos = await httpClient.GetFromJsonAsync<IEnumerable<ToDoOutputDto>>("/api/ToDos/pending");
+        pendingToDos.Should().NotBeNull();
+        pendingToDos.Should().HaveCount(1);
+
+        HttpResponseMessage todoPatchResponse = await httpClient.PatchAsJsonAsync($"/api/ToDos/{postedToDo.Id}", ToDoTestData.TODO_PATCH_COMPLETED);
+        todoPatchResponse.StatusCode.Should().Be(HttpStatusCode.OK);
+        todoPatchResponse.Content.Should().NotBeNull();
+        ToDoOutputDto? patchedToDo = await todoPatchResponse.Content.ReadFromJsonAsync<ToDoOutputDto>();
+        patchedToDo.Should().NotBeNull();
+        patchedToDo.Id.Should().Be(postedToDo.Id);
+        patchedToDo.IsCompleted.Should().BeTrue();
+
+        returnedToDos = await httpClient.GetFromJsonAsync<IEnumerable<ToDoOutputDto>>("/api/ToDos");
+        returnedToDos.Should().NotBeNull();
+        returnedToDos.Should().HaveCount(2);
+
+        pendingToDos = await httpClient.GetFromJsonAsync<IEnumerable<ToDoOutputDto>>("/api/ToDos/pending");
+        pendingToDos.Should().NotBeNull();
+        pendingToDos.Should().HaveCount(0);
     }
 }
---
+++ ToDoListService.TestsIntegration/TestData/ToDoTestData.cs
@@ -0,0 +1,57 @@
+﻿using ToDoListService.Domain.Dtos;
+
+namespace ToDoListService.TestsIntegration.TestData;
+
+public static class ToDoTestData
+{
+
+    #region ToDoInputDto
+    public static ToDoInputDto TODO_1_INPUT => new ToDoInputDto()
+    {
+        Title = "Check the plants!",
+        Description = "Give some water to the plants",
+        IsCompleted = false,
+        DueDate = DateOnly.FromDateTime(DateTime.Now.AddDays(15))
+    };
+
+    public static ToDoInputDto TODO_2_INPUT => new ToDoInputDto()
+    {
+        Title = "Check the water!",
+        Description = "Put some water in the Tank",
+        IsCompleted = false,
+        DueDate = DateOnly.FromDateTime(DateTime.Now.AddDays(15))
+    };
+
+    public static ToDoInputDto TODO_3_INPUT => new ToDoInputDto()
+    {
+        Title = "Feed the dog",
+        Description = "Give some food to the dog",
+        IsCompleted = false,
+        DueDate = DateOnly.FromDateTime(DateTime.Now.AddDays(15))
+    };
+
+    public static ToDoInputDto TODO_4_INPUT => new ToDoInputDto()
+    {
+        Title = "Check dog food",
+        Description = "If there is no enough food we have to buy some dog food",
+        IsCompleted = false,
+        DueDate = DateOnly.FromDateTime(DateTime.Now.AddDays(15))
+    };
+    #endregion
+
+    #region ToDoPatchDto
+    public static ToDoPatchDto TODO_PATCH_COMPLETED => new ToDoPatchDto() { IsCompleted = true };
+    public static ToDoPatchDto TODO_PATCH_INCOMPLETE => new ToDoPatchDto() { IsCompleted = false };
+    #endregion
+
+    #region ToDoOutputDto
+    public static ToDoOutputDto TODO_1_OUTPUT_COMPLETED => new ToDoOutputDto()
+    {
+        Id = 1,
+        Title = "Check the plants!",
+        Description = "Give some water to the plants",
+        IsCompleted = true,
+        DueDate = DateOnly.FromDateTime(DateTime.Now.AddDays(15))
+    };
+    #endregion
+}
